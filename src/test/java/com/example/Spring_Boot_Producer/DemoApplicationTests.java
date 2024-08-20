4c47e68914fab0b1363cb01b20390c8d38fceae3
swapnil-khade <swapnil.khade@hotmail.com>
2024-08-20 13:48:56 +0530

    Initial commit

---
+++ src/test/java/com/example/Spring_Boot_Producer/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_Producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
